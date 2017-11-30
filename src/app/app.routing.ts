99be7ff1660d11d7c2617f59a58346ef4f4a8eb1
ragnerrok <rayagner@gmail.com>
2017-11-30 00:41:13 -0600

    forgot to add the children to checkout

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { ShowroomComponent } from "./showroom/showroom.component";
 import { CheckoutComponent } from "./checkout/checkout.component";
 import { CANDY_ROUTES } from "./showroom/candy.routes";
-
+import { CHECKOUT_ROUTES } from "./checkout/checkout.routes";
 
 const APP_ROUTES: Routes = [
     { path: '', redirectTo: '/showroom', pathMatch: 'full' },
     { path: 'showroom', component: ShowroomComponent, children: CANDY_ROUTES },
-    { path: 'checkout', component: CheckoutComponent }
+    { path: 'checkout', component: CheckoutComponent, children: CHECKOUT_ROUTES }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
