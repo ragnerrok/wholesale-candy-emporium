212f2b7d91e1da8e58ef8abdb1ab79fad49e4306
ragnerrok <rayagner@gmail.com>
2017-11-30 00:15:08 -0600

    updating product page

--- src/app/showroom/candy-product-page.component.ts
+++ src/app/showroom/candy-product-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Candy } from "./candy.model";
 import { CandyService } from "./candy.service";
+import { CartItemsService } from "../cart/cart-items.service";
 
 @Component({
   selector: 'app-candy-product-pages',
@@ -11,12 +12,15 @@ export class CandyProductPageComponent implements OnInit {
   //this is where I would start having the app go ask the backend "hey what candy I got?"
   candy: Candy;
 
-  constructor(private candyService: CandyService) {}
+  constructor(private cartItemsService: CartItemsService, private candyService: CandyService) {}
 
   ngOnInit() {
     this.candy = this.candyService.getChosenCandy();
   }
 
-
+  onSubmit(amount: number) {
+    console.log(amount);
+    this.cartItemsService.addCandyToCart(this.candy, amount);
+  }
 
 }
