9e32c5b1cb612997aa3cfda04650e357dd960d2e
ragnerrok <rayagner@gmail.com>
2017-11-29 23:36:41 -0600

    updating cards to look nice

--- src/app/showroom/candy.model.ts
+++ src/app/showroom/candy.model.ts
@@ -2,5 +2,7 @@ export class Candy {
   constructor(public name: string,
               public size: string,
               public price: number,
-              public inventory: number) {}
+              public inventory: number,
+              public image: string,
+              public description: string) {}
 }
--- src/app/showroom/candy.service.ts
+++ src/app/showroom/candy.service.ts
@@ -3,14 +3,14 @@ export class CandyService {
   private chosenCandy: Candy;
   private candies: Candy[] = [
     //name, size, price
-    new Candy('Fundo Bar', 'Small', 1, 100),
-    new Candy('Fundo Bar', 'Medium', 1.5, 100),
-    new Candy('Fundo Bar', 'Large', 3, 100),
+    new Candy('Fundo Bar', 'Small', 1, 100, 'fundo-bar-small.png', 'These bars are packed with caramel nougat goodness. They come in three fun sizes to help get you through your day!'),
+    new Candy('Fundo Bar', 'Medium', 1.5, 100, 'fundo-bar-medium.png', 'These bars are packed with caramel nougat goodness. They come in three fun sizes to help get you through your day!'),
+    new Candy('Fundo Bar', 'Large', 3, 100, 'fundo-bar-large.png', 'These bars are packed with caramel nougat goodness. They come in three fun sizes to help get you through your day!'),
 
-    new Candy('Extreme Micro Sours', '', 1, 100),
+    new Candy('Extreme Micro Sours', '', 1, 100, 'extreme-micro-sours.png', 'Even the tiniest micro sour will leave you squirming in your shoes. Be careful with these, you sour patch youngsters.'),
 
-    new Candy('Solo Brand Twirly Ropes', 'Small', 10, 100),
-    new Candy('Solo Brand Twirly Ropes', 'Medium', 12.75, 100)
+    new Candy('Solo Brand Twirly Ropes', 'Small', 10, 100, 'solo-brand-twirly-ropes-small.png', 'An individual clamshell packaged string o’ licorice. This string is so tasty, you might spend an entire day trying to open a dozen!'),
+    new Candy('Solo Brand Twirly Ropes', 'Medium', 12.75, 100, 'solo-brand-twirly-ropes-medium.png', 'An individual clamshell packaged string o’ licorice. This string is so tasty, you might spend an entire day trying to open a dozen!')
 
   ];
 
