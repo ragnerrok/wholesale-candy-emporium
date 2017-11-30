25810b28a61960fe34a0a356651f4a4d068559d2
ragnerrok <rayagner@gmail.com>
2017-11-29 23:36:01 -0600

    need to add total componet to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,11 @@ import { AppComponent } from './app.component';
 import { ShowroomComponent } from './showroom/showroom.component';
 import { ShowroomCandyCardsComponent } from './showroom/showroom-candy-cards.component';
 import { ShowroomCandyCardComponent } from './showroom/showroom-candy-card.component';
+import { CandyProductPageComponent } from './showroom/candy-product-page.component';
+
 import { CheckoutComponent } from './checkout/checkout.component';
 import { CartComponent } from './cart/cart.component';
+import { CartTotalComponent } from './cart/cart-total.component';
 import { CartItemComponent } from './cart/cart-item.component';
 
 import { CandyService } from './showroom/candy.service';
@@ -22,8 +25,11 @@ import { routing } from "./app.routing";
     ShowroomComponent,
     ShowroomCandyCardsComponent,
     ShowroomCandyCardComponent,
+    CandyProductPageComponent,
+
     CheckoutComponent,
     CartComponent,
+    CartTotalComponent,
     CartItemComponent
   ],
   imports: [ BrowserModule, routing],
