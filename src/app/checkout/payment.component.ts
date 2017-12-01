dc525991cca5089553b7b49232df28d6205e4875
ragnerrok <rayagner@gmail.com>
2017-11-30 21:56:11 -0600

    forgot to remove function call

--- src/app/checkout/payment.component.ts
+++ src/app/checkout/payment.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
   template: `
   <h4 class="spacing-on-top"> Payment </h4>
   <img class="img-responsive" style="max-width: 300px" src="../assets/girly-candy.png" alt="Money image">
-  <div><button class="btn btn-primary" [routerLink]="['../shipping']" (click)="onCompleteOrder()">Click to Add Money!</button></div>
+  <div><button class="btn btn-primary" [routerLink]="['../shipping']">Click to Add Money!</button></div>
   `,
 })
 export class PaymentComponent {
--- src/app/checkout/shipping.component.ts
+++ src/app/checkout/shipping.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
   template: `
   <h4 class="spacing-on-top"> Shipping </h4>
   <img class="img-responsive" style="max-width: 300px" src="../assets/girly-candy.png" alt="Money image">
-  <div><button class="btn btn-primary" [routerLink]="['../preview']" (click)="onCompleteOrder()">Click to Add Shipping Address!</button></div>
+  <div><button class="btn btn-primary" [routerLink]="['../preview']">Click to Add Shipping Address!</button></div>
   `,
 })
 export class ShippingComponent {
